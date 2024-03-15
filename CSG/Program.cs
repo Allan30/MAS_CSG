e1d109b92cedb85c1c2e591a428c6cca672c2fa8
Allan <Epulapp@EPULAPP2103>
2024-03-15 17:44:22 +0100

    last push I hope

--- CSG/Program.cs
+++ CSG/Program.cs
@@ -10,7 +10,7 @@ public class Program
         [
             new Car("car1", 90, 4),
             new Car("car2", 80, 3),
-            new Car("car3", 90, 5)
+            new Car("car3", 200, 5)
         ];
         List<Goal> goals =
         [
@@ -20,16 +20,30 @@ public class Program
             new Goal("agent4", "Paris", "Lyon", new DateTime(2024, 12, 25, 20, 50, 00), DateTime.Now),
             new Goal("agent5", "Lyon", "Paris", new DateTime(2024, 12, 26, 10, 30, 00), DateTime.Now),
             new Goal("agent6", "Lyon", "Paris", new DateTime(2024, 12, 26, 10, 30, 00), DateTime.Now),
-            new Goal("agent7", "Marseille", "Lyon", new DateTime(2024, 12, 20, 10, 30, 00), DateTime.Now),
+            new Goal("agent7", "Marseille", "Lyon", new DateTime(2024, 12, 25, 10, 40, 00), DateTime.Now),
             new Goal("agent8", "Paris", "Lyon", new DateTime(2024, 12, 25, 10, 20, 00), DateTime.Now),
             new Goal("agent9", "Paris", "Lyon", new DateTime(2024, 12, 25, 10, 40, 00), DateTime.Now),
         ];
 
-        var algorithm = new IPAlgorithm(goals, cars);
+        var algorithm = new DPAlgorithm(goals, cars);
         
         var coalitions = algorithm.Start();
 
-        Console.WriteLine("=======================================================");
+        Console.WriteLine("====================== DP Alogrithm =================================");
+        foreach (var coalition in coalitions)
+        {
+            Console.WriteLine("Coalition for car " + coalition[0].Car?.Name + ": ");
+            foreach (var agent in coalition)
+            {
+                Console.WriteLine(agent.Name);
+            }
+        }
+        
+        algorithm = new DPAlgorithm(goals, cars);
+        
+        coalitions = algorithm.Start();
+        
+        Console.WriteLine("====================== IP Alogrithm =================================");
         foreach (var coalition in coalitions)
         {
             Console.WriteLine("Coalition for car " + coalition[0].Car?.Name + ": ");
